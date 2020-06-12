3cbfc29e05527039a2eeacf70de33923fdaa0233
shakalya195 <shakalya195@gmail.com>
2020-06-12 10:14:35 +0530

    final

--- src/app/blocks/block-blog/block-blog.component.ts
+++ src/app/blocks/block-blog/block-blog.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-block-blog',
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlockBlogComponent implements OnInit {
 
+  @Input() post:any;
   constructor() { }
 
   ngOnInit() {
